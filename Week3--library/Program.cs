4a1b44944f1307ff98ae058b27c00f8d4fd858af
Maciej Chroszcz <Chroszcz-M@ulster.ac.uk>
2025-10-07 13:02:12 +0100

    Updated code with a new Member class.

--- Week3--library/Program.cs
+++ Week3--library/Program.cs
@@ -3,10 +3,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create a new instance (object) of the Book class
-        // Note how the object name differs from the class name
         Book book = new Book("C# for beginners", "Bill Gates", "1234567");
-
+        Book book1 = new Book("Ultimate C#", "Microsoft", "2233445");
+        Console.WriteLine("Currently available books");
         book.DisplayInfo();
+        book1.DisplayInfo();
+
+        member member = new member(1, "John Smith", "1 High Street", 0790090090);
+        member member1 = new member(2, "Mary Jones", "102 Garden Road", 0790345666);
+
+        Console.WriteLine("Current library members");
+        member.DisplayInfo();
+        member1.DisplayInfo();
     }
 }
---
+++ Week3--library/member.cs
@@ -0,0 +1,49 @@
+﻿namespace Library
+{
+    internal class member
+    {
+        private int memberId;
+        private string name;
+        private string address;
+        private int phone;
+
+        // Public properties
+        public int MemberId
+        {
+            get { return memberId; }
+            private set { memberId = value; } // Private setter makes it read-only
+        }
+        public string Name
+        {
+            get { return name; }  // get method
+            set { name = value; } // set method
+        }
+        public string Address
+        {
+            get { return address; }  // get method
+            set { address = value; } // set method
+        }
+        public int Phone
+        {
+            get { return phone; }  // get method
+            set { phone = value; } // set method
+        }
+
+        // Constructor for new member
+        public member(int memberId, string name, string address, int phone)
+        {
+            MemberId = memberId;
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Member ID: {memberId}");
+            Console.WriteLine($"Member Name: {name}");
+            Console.WriteLine($"Member Address: {address}");
+            Console.WriteLine($"Member Phone: {phone}");
+            Console.WriteLine();
+        }
+    }
+}
