e4936145979f7c5f20325874f6903e7f9ed54138
Maciej Chroszcz <Chroszcz-M@ulster.ac.uk>
2025-10-07 12:54:57 +0100

    Updated code with variables and properties.

--- Week3--library/Book.cs
+++ Week3--library/Book.cs
@@ -2,9 +2,25 @@ namespace Library
 {
     class Book
     {
-        string title;
-        string author;
-        string isbn;
+        private string title;
+        private string author;
+        private string isbn;
+
+        public string Title
+        {
+            get { return title; }  // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return author; }
+            set { author = value; }
+        }
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
 
         public void DisplayInfo()
         {
